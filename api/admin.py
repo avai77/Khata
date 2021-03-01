8d64436535b0a59fc586f7067248345d5c0c9ef9
Aidin <bekturganovaidin@gmail.com>
2021-03-01 16:48:45 +0600

    Done with Models, added admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Advertisement
 
 # Register your models here.
 
@@ -9,3 +9,8 @@ from .models import Category
 class CategoryModel(admin.ModelAdmin):
     list_filter = ('typeOfAd', 'active')
     list_display = ('typeOfAd', 'active')
+
+@admin.register(Advertisement)
+class AdvertisementModel(admin.ModelAdmin):
+     list_filter = ('title', 'image', 'description', 'phone', 'price', 'category', 'address', 'year', 'area', 'active', 'created')
+     list_display = ('title', 'image', 'description', 'phone', 'price', 'category', 'address', 'year', 'area', 'active', 'created')
