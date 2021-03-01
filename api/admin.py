6cb72c16fad627a2220d925bc2e21aaa87f04e90
Aidin <bekturganovaidin@gmail.com>
2021-03-01 16:34:37 +0600

    Basic models, admin configuration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+
+# admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryModel(admin.ModelAdmin):
+    list_filter = ('typeOfAd', 'active')
+    list_display = ('typeOfAd', 'active')
--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+class Category(models.Model):
+    typeOfAd = models.CharField(max_length=250)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.typeOfAd
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
