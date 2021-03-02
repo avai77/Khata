281407b08db31a5805733e46c39795e5b095fecb
avai7 <aydarov1582@gmail.com>
2021-03-02 11:08:01 +0600

    API Decorators, DELETE PUT METHODS ADDED

--- api/views.py
+++ api/views.py
@@ -3,34 +3,87 @@ from .models import Category, Advertisement
 from .serializers import CategorySerializer, AdvertisementSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
+'''
+@api_view(['GET', 'POST'])
 def category_list(request):
     if request.method == 'GET':
         categories = Category.objects.all()
         serializers = CategorySerializer(categories, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializers = CategorySerializer(data=data)
+        serializers = CategorySerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
-        return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'POST'])
 def advertisement_list(request):
     if request.method == 'GET':
         advertisement = Advertisement.objects.all()
         serializers = AdvertisementSerializer(advertisement, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializers = AdvertisementSerializer(data=data)
+        serializers = AdvertisementSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
-        return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def category_details(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+
+    except Category.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def advertisement_details(request, pk):
+    try:
+        advertisement = Advertisement.objects.get(pk=pk)
+
+    except advertisement.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AdvertisementSerializer(advertisement)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AdvertisementSerializer(advertisement, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        advertisement.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    '''
+
+
+
 
 
 
