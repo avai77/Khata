e807f4d0b986e17602cf6458fa7e90a47d8b349a
avai7 <aydarov1582@gmail.com>
2021-03-11 23:48:25 +0600

    Cleaned All unused references

--- api/views.py
+++ api/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
-from rest_framework.viewsets import ViewSet
 
 from .models import Category, Advertisement
-from .serializers import CategorySerializer, AdvertisementSerializer, UploadSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import APIView
-from rest_framework import generics
-from rest_framework import mixins
+from .serializers import CategorySerializer, AdvertisementSerializer
+
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 
@@ -21,14 +15,3 @@ class AdvertisementViewSet(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     authentication_classes = (TokenAuthentication,)
-    #HELLO
-
-
-
-
-
-
-
-
-
-
