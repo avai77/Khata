a9066f3cb7704b50bdd5dafec0601194164d8db5
Aidin <bekturganovaidin@gmail.com>
2021-03-11 17:19:08 +0600

    Token Auth and Model View Sets

--- api/views.py
+++ api/views.py
@@ -9,150 +9,24 @@ from rest_framework.decorators import APIView
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 
-class CategoryViewSet(viewsets.ViewSet):
-    def list(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset = Category.objects.all()
-        category = get_object_or_404(queryset, pk=pk)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        category = Category.objects.get(pk=pk)
-
-        serializer = CategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        category = Category.objects.get(pk=pk)
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class UploadViewSet(ViewSet):
-    serializer_class = UploadSerializer
-
-    def list(self, request):
-        return Response("GET API")
-
-    def create(self, request):
-        file_uploaded = request.FILES.get('file_uploaded')
-        content_type = file_uploaded.content_type
-        response = "POST API and you have uploaded a {} file".format(content_type)
-        return Response(response)
-
-
-class AdvertisementViewSet(viewsets.ViewSet):
-
-
-    def list(self, request):
-        advertisement = Advertisement.objects.all()
-        serializer = AdvertisementSerializer(advertisement, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer_class = AdvertisementSerializer
-        file_uploaded = request.FILES.get('file_uploaded')
-        content_type = file_uploaded.content_type
-        response = "POST API and you have uploaded a {} file".format(content_type)
-        return Response(response)
-        # serializer = AdvertisementSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset = Advertisement.objects.all()
-        advertisement = get_object_or_404(queryset, pk=pk)
-        serializer = AdvertisementSerializer(advertisement)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        advertisement = Advertisement.objects.get(pk=pk)
-
-        serializer = AdvertisementSerializer(advertisement, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        advertisement = Advertisement.objects.get(pk=pk)
-        advertisement.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-'''
-class CategoryList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    authentication_classes = (TokenAuthentication,)
 
-    def get(self, request):
-        return self.list(request)
-
-    def post(self, request):
-        return self.create(request)
-
-
-class CategoryDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-    lookup_field = 'id'
-
-    def get(self, request, id):
-        return self.retrieve(request, id=id)
-
-    def put(self, request, id):
-        return self.update(request, id=id)
-
-    def delete(self, request, id):
-        return self.destroy(request, id=id)
-
-class AdvertisementList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class AdvertisementViewSet(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-
-    def get(self, request):
-        return self.list(request)
-
-    def post(self, request):
-        return self.create(request)
+    authentication_classes = (TokenAuthentication,)
 
 
-class AdvertisementDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-    queryset = Advertisement.objects.all()
-    serializer_class = AdvertisementSerializer
 
-    lookup_field = 'id'
 
-    def get(self, request, id):
-        return self.retrieve(request, id=id)
 
-    def put(self, request, id):
-        return self.update(request, id=id)
 
-    def delete(self, request, id):
-        return self.destroy(request, id=id)
-    
-    '''
 
 
 
