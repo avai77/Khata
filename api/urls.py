18259aa2b5a3788fedefe2e90d8305b0ce81b101
avai7 <aydarov1582@gmail.com>
2021-03-18 10:11:44 +0600

    Changed link for api

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,6 @@ router.register('ads', AdvertisementViewSet, basename='ads')
 router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('/', include(router.urls)),
+    path('', include(router.urls)),
 
 ]
