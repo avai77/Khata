7129cd6d2f502af476872ef7ddd72206146e06ad
Aidin <bekturganovaidin@gmail.com>
2021-04-22 15:01:23 +0600

    CORS

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
+
 from .models import Category, Advertisement
 from .serializers import CategorySerializer, AdvertisementSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from rest_framework import filters
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -11,11 +14,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
+
+class AdsSearch(viewsets.ModelViewSet):
+    search_fields = ['title']
+    filter_backends = (filters.SearchFilter,)
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+
 class AdvertisementViewSet(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/avai7/PycharmProjects/Khata/venv"
+set -gx VIRTUAL_ENV "/Users/user/Documents/pythonKhat/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
